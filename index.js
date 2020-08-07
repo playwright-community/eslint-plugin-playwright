dd2d01bea36ecb97140039371a4991aa965e7b6e
mmarkelov <maks-markel@mail.ru>
2020-08-07 14:33:22 +0300

    Add rules for helper functions

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ module.exports = {
         'shared-node-browser': true,
         jest: true,
       },
+      rules: {
+        'jest/no-standalone-expect': [
+          'error',
+          { 'additionalTestBlockFunctions':
+            ['test.jestPlaywrightDebug',
+              'it.jestPlaywrightDebug',
+              'test.jestPlaywrightSkip',
+              'it.jestPlaywrightSkip',
+              'test.jestPlaywrightConfig',
+              'it.jestPlaywrightConfig']
+          }
+      ]},
       globals: {
         browserName: true,
         deviceName: true,
