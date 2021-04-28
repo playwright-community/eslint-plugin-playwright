02a5062ed70315c9d9b3959d988e75be0e23eab6
Mark Skelton <mdskelton99@gmail.com>
2021-04-28 17:14:11 -0500

    feat: add custom missing await rule (#4)

---
+++ test/missing-playwright-await.spec.js
@@ -0,0 +1,77 @@
+const { RuleTester } = require("eslint");
+const rule = require("../lib/rules/missing-playwright-await");
+
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+});
+
+const wrapInTest = (input) => `test('a', async () => { ${input} })`;
+
+const invalid = (code, output, options = []) => ({
+  code: wrapInTest(code),
+  errors: ["Playwright expectations must be awaited or returned."],
+  options,
+  output: wrapInTest(output),
+});
+
+const valid = (code, options = []) => ({
+  code: wrapInTest(code),
+  options,
+});
+
+const options = [{ customMatchers: ["toBeCustomThing"] }];
+
+const defaultMatchers = [
+  "toHaveSelector",
+  "toHaveSelectorCount",
+  "toHaveText",
+  "toEqualText",
+  "toEqualValue",
+  "toEqualUrl",
+  "toHaveFocus",
+];
+
+new RuleTester().run("missing-playwright-await", rule, {
+  invalid: [
+    // Default matchers
+    ...defaultMatchers.flatMap((matcher) => [
+      invalid(
+        `expect(page).${matcher}("text")`,
+        `await expect(page).${matcher}("text")`
+      ),
+      invalid(
+        `expect(page).not.${matcher}("text")`,
+        `await expect(page).not.${matcher}("text")`
+      ),
+    ]),
+
+    // Custom matchers
+    invalid(
+      `expect(page).toBeCustomThing(false)`,
+      `await expect(page).toBeCustomThing(false)`,
+      options
+    ),
+    invalid(
+      `expect(page).not.toBeCustomThing(true)`,
+      `await expect(page).not.toBeCustomThing(true)`,
+      options
+    ),
+  ],
+  valid: [
+    // Default matchers
+    ...defaultMatchers.flatMap((matcher) => [
+      valid(`await expect(page).${matcher}("text")`),
+      valid(`await expect(page).not.${matcher}("text")`),
+      valid(`return expect(page).${matcher}("text")`),
+    ]),
+
+    // Custom matchers
+    valid("await expect(page).toBeCustomThing(true)", options),
+    valid("await expect(page).toBeCustomThing(true)", options),
+    valid("await expect(page).toBeCustomThing(true)", options),
+    valid("await expect(page).toBeCustomThing(true)"),
+    valid("expect(page).toBeCustomThing(true)"),
+  ],
+});
