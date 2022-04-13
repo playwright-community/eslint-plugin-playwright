ab73a2e8a4823bf259c63409bbafac692e1a74c8
Yevhen Laichenkov <elaichenkov@gmail.com>
2022-04-13 18:58:14 +0300

    feat: add noSkippedTest rule

--- README.md
+++ README.md
@@ -190,3 +190,38 @@ test.describe.serial('two tests in serial mode', () => {
   test('two', async ({ page }) => {});
 });
 ```
+
+### `no-skipped-test`
+
+Disallow usage of `.skip()` annotation
+
+Examples of **incorrect** code for this rule:
+
+```js
+test.skip('skip this test', async ({ page }) => {});
+
+test.describe.skip('skip two tests', () => {
+  test('one', async ({ page }) => {});
+  test('two', async ({ page }) => {});
+});
+
+test.describe('skip test inside describe', () => {
+  test.skip();
+});
+
+test.describe('skip test conditionally', async ({ browserName }) => {
+  test(browserName === 'firefox', 'Working on it');
+});
+
+```
+
+Examples of **correct** code for this rule:
+
+```js
+test('this test', async ({ page }) => {});
+
+test.describe('two tests', () => {
+  test('one', async ({ page }) => {});
+  test('two', async ({ page }) => {});
+});
+```
---
+++ test/no-skipped-test.spec.js
@@ -0,0 +1,42 @@
+const { RuleTester } = require('eslint');
+const rule = require('../lib/rules/no-skipped-test');
+
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+});
+
+const invalid = (code, output) => ({
+  code,
+  errors: [
+    {
+      messageId: 'noSkippedTest',
+      suggestions: [{ messageId: 'removeSkippedTestAnnotation', output }],
+    },
+  ],
+});
+
+new RuleTester().run('no-skipped-test', rule, {
+  invalid: [
+    invalid('test.skip("skip this test", async ({ page }) => {});', 'test("skip this test", async ({ page }) => {});'),
+    invalid('test.describe.skip("skip this describe", () => {});', 'test.describe("skip this describe", () => {});'),
+    invalid('test.skip(browserName === "firefox", "Still working on it");', ''),
+    invalid(
+      'test.describe.parallel("run in parallel", () => { test.skip(); expect(true).toBe(true); })',
+      'test.describe.parallel("run in parallel", () => {  expect(true).toBe(true); })'
+    ),
+    invalid('test.skip();', ''),
+    invalid('test.skip()', ''),
+  ],
+  valid: [
+    'test.describe("describe tests", () => {});',
+    'test.describe("describe tests", () => {});',
+    'test("one", async ({ page }) => {});',
+    'test.only(isMobile, "Settings page does not work in mobile yet");',
+    'test.slow();',
+    'const skip = true;',
+    'function skip() { return null };',
+    'this.skip();',
+  ],
+});
