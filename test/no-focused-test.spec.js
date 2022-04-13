8702d09afa75f4b410753ed67114326cdc800433
Yevhen Laichenkov <elaichenkov@gmail.com>
2022-04-13 16:06:11 +0300

    chore: remove unneeded function in test. change DS for isTestGroup function

--- test/no-focused-test.spec.js
+++ test/no-focused-test.spec.js
@@ -7,63 +7,57 @@ RuleTester.setDefaultConfig({
   },
 });
 
-const wrapInTest = (name, input) => `test('verify noFocusedTest rule with ${name}', async () => { ${input} })`;
-
-const invalid = (name, code, output) => ({
-  code: wrapInTest(name, code),
+const invalid = (code, output) => ({
+  code,
   errors: [
     {
       messageId: 'noFocusedTest',
-      suggestions: [{ messageId: 'removeFocusedTestAnnotation', output: wrapInTest(name, output) }],
+      suggestions: [{ messageId: 'removeFocusedTestAnnotation', output }],
     },
   ],
 });
 
-const valid = (name, code) => ({
-  code: wrapInTest(name, code),
+const valid = (code) => ({
+  code,
 });
 
 new RuleTester().run('no-focused-test', rule, {
   invalid: [
     invalid(
-      'test.describe.only',
       'test.describe.only("skip this describe", () => {});',
       'test.describe("skip this describe", () => {});'
     ),
 
     invalid(
-      'test.describe.parallel.only',
       'test.describe.parallel.only("skip this describe", () => {});',
       'test.describe.parallel("skip this describe", () => {});'
     ),
 
     invalid(
-      'test.describe.serial.only',
       'test.describe.serial.only("skip this describe", () => {});',
       'test.describe.serial("skip this describe", () => {});'
     ),
 
     invalid(
-      'test.only',
       'test.only("skip this test", async ({ page }) => {});',
       'test("skip this test", async ({ page }) => {});'
     ),
   ],
   valid: [
-    valid('test.describe without only annotation', 'test.describe("describe tests", () => {});'),
+    valid('test.describe("describe tests", () => {});'),
 
-    valid('test.describe with skip annotation', 'test.describe.skip("describe tests", () => {});'),
+    valid('test.describe.skip("describe tests", () => {});'),
 
-    valid('test without only annotation', 'test("one", async ({ page }) => {});'),
+    valid('test("one", async ({ page }) => {});'),
 
-    valid('test.fixme', 'test.fixme(isMobile, "Settings page does not work in mobile yet");'),
+    valid('test.fixme(isMobile, "Settings page does not work in mobile yet");'),
 
-    valid('test.slow', 'test.slow();'),
+    valid('test.slow();'),
 
-    valid('constant only', 'const only = true;'),
+    valid('const only = true;'),
 
-    valid('function only', 'function only() { return null };'),
+    valid('function only() { return null };'),
 
-    valid('method only', 'this.only();'),
+    valid('this.only();'),
   ],
 });
