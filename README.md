f4a412ec3c4ac0c58e80ebdadff13e0cbc84bf3b
Jesse Mazzella <jessemazzella@gmail.com>
2022-06-10 12:53:00 -0700

    Add `no-conditional-in-test` rule, tests, and docs

--- README.md
+++ README.md
@@ -319,3 +319,79 @@ The rule accepts a non-required option to override the default maximum nested de
   ]
 }
 ```
+
+### `no-conditional-in-test`
+
+Disallow conditional statements such as `if`, `switch`, and ternary expressions within tests.
+
+Examples of **incorrect** code for this rule:
+
+```js
+test('foo', async ({ page }) => {
+  if (someCondition) {
+    bar();
+  }
+});
+
+test('bar', async ({ page }) => {
+  switch (mode) {
+    case 'single':
+      generateOne();
+      break;
+    case 'double':
+      generateTwo();
+      break;
+    case 'multiple':
+      generateMany();
+      break;
+  }
+
+  await expect(page.locator('.my-image').count()).toBeGreaterThan(0);
+});
+
+test('baz', async ({ page }) => {
+  const hotkey = process.platform === 'linux' ? ['Control', 'Alt', 'f'] : ['Alt', 'f'];
+  await Promise.all(hotkey.map(x => page.keyboard.down(x)))
+
+  expect(actionIsPerformed()).toBe(true);
+});
+```
+
+Examples of **correct** code for this rule:
+
+```js
+test.describe('my tests', () => {
+  if (someCondition) {
+    test('foo', async ({ page }) => {
+      bar();
+    });
+  }
+});
+
+beforeEach(() => {
+  switch (mode) {
+    case 'single':
+      generateOne();
+      break;
+    case 'double':
+      generateTwo();
+      break;
+    case 'multiple':
+      generateMany();
+      break;
+  }
+});
+
+test('bar', async ({ page }) => {
+  await expect(page.locator('.my-image').count()).toBeGreaterThan(0);
+})
+
+const hotkey = process.platform === 'linux' ? ['Control', 'Alt', 'f'] : ['Alt', 'f'];
+
+test('baz', async ({ page }) => {
+  await Promise.all(hotkey.map(x => page.keyboard.down(x)))
+
+  expect(actionIsPerformed()).toBe(true);
+});
+
+```
---
+++ test/no-conditional-in-test.spec.js
@@ -0,0 +1,231 @@
+const { runRuleTester } = require('../lib/utils/rule-tester');
+const rule = require('../lib/rules/no-conditional-in-test');
+
+const invalid = (code, errors) => ({
+  code,
+  errors: errors || [
+    {
+      messageId: 'conditionalInTest',
+    },
+  ],
+});
+
+const valid = (code) => ({
+  code
+});
+
+runRuleTester('no-conditional-in-test', rule, {
+  invalid: [
+    invalid('test("foo", () => { if (true) { expect(1).toBe(1); } });'),
+    invalid(`
+      test.describe("foo", () => {
+        test("bar", () => {
+          if (someCondition()) {
+            expect(1).toBe(1);
+          } else {
+            expect(2).toBe(2);
+          }
+        });
+      });
+    `),
+    invalid(`
+      describe('foo', () => {
+        test('bar', () => {
+          if ('bar') {}
+        })
+      })
+    `),
+    invalid(`
+      describe('foo', () => {
+        test('bar', () => {
+          if ('bar') {}
+        })
+        test('baz', () => {
+          if ('qux') {}
+          if ('quux') {}
+        })
+      })
+    `, 
+    [
+      {
+        messageId: 'conditionalInTest',
+      },
+      {
+        messageId: 'conditionalInTest',
+      },
+      {
+        messageId: 'conditionalInTest',
+      },
+    ]),
+    invalid(`
+      test("foo", function () {
+        switch(someCondition()) {
+          case 1:
+            expect(1).toBe(1);
+            break;
+          case 2:
+            expect(2).toBe(2);
+            break;
+        }
+      });
+    `),
+    invalid(`
+      test('foo', () => {
+        if ('bar') {}
+      })
+    `),
+    invalid(`
+      test('foo', () => {
+        bar ? expect(1).toBe(1) : expect(2).toBe(2);
+      })
+    `),
+    invalid(`
+      test('foo', function () {
+        bar ? expect(1).toBe(1) : expect(2).toBe(2);
+      })
+    `),
+    invalid(`
+      test('foo', function () {
+        if ('bar') {}
+      })
+    `),
+    invalid(`
+      test('foo', async function ({ page }) {
+        await asyncFunc();
+        if ('bar') {}
+      })
+    `),
+    invalid(`
+      test.skip('foo', () => {
+        if ('bar') {}
+      })
+    `),
+    invalid(`
+      test.skip('foo', async ({ page }) => {
+        await asyncFunc();
+        if ('bar') {}
+      })
+    `),
+    invalid(`
+      test.skip('foo', function () {
+        if ('bar') {}
+      })
+    `),
+    invalid(`
+      test.only('foo', () => {
+        if ('bar') {}
+      })
+    `),
+    invalid(`
+      test.fixme('foo', () => {
+        if ('bar') {}
+      })
+    `),
+    invalid(`
+      test('foo', () => {
+        callExpression()
+        if ('bar') {}
+      })
+    `),
+    invalid(`
+      testDetails.forEach((detail) => {
+        test(detail.name, () => {
+          if (detail.fail) {
+            test.fail();
+          } else {
+            expect(true).toBe(true);
+          }
+        })
+      });
+    `)
+  ],
+  valid: [
+    'test("foo", () => { expect(1).toBe(1); });',
+    'test.fixme("some broken test", () => { expect(1).toBe(1); });',
+    'test.describe("foo", () => { if(true) { test("bar", () => { expect(true).toBe(true); }); } });',
+    'test.skip(process.env.APP_VERSION === "v1", "There are no settings in v1");',
+    'test("some slow test", () => { test.slow(); })',
+    'const foo = bar ? foo : baz;',
+    valid(`
+      test.describe('foo', () => {
+        test.afterEach(() => {
+          if ('bar') {}
+        });
+      })
+    `),
+    valid(`
+      test.describe('foo', () => {
+        test.beforeEach(() => {
+          if ('bar') {}
+        });
+      })
+    `),
+    valid(`
+      test.describe('foo', function () {
+        test.beforeEach(function () {
+          if ('bar') {}
+        });
+      })
+    `),
+    valid(`
+      test.describe.parallel('foo', function () {
+        test.beforeEach(function () {
+          if ('bar') {}
+        });
+      })
+    `),
+    valid(`
+      test.describe.parallel.only('foo', function () {
+        test.beforeEach(function () {
+          if ('bar') {}
+        });
+      })
+    `),
+    valid(`
+      test.describe.serial('foo', function () {
+        test.beforeEach(function () {
+          if ('bar') {}
+        });
+      })
+    `),
+    valid(`
+      test.describe.serial.only('foo', function () {
+        test.beforeEach(function () {
+          if ('bar') {}
+        });
+      })
+    `),
+    valid(`
+      const values = something.map((thing) => {
+        if (thing.isFoo) {
+          return thing.foo
+        } else {
+          return thing.bar;
+        }
+      });
+
+      test.describe('valid', () => {
+        test('still valid', () => {
+          expect(values).toStrictEqual(['foo']);
+        });
+      });
+    `),
+    valid(`
+      describe('valid', () => {
+        const values = something.map((thing) => {
+          if (thing.isFoo) {
+            return thing.foo
+          } else {
+            return thing.bar;
+          }
+        });
+
+        test.describe('still valid', () => {
+          test('really still valid', () => {
+            expect(values).toStrictEqual(['foo']);
+          });
+        });
+      });
+    `)
+  ]
+});
