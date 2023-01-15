b324d3a0594b70488da7f0f65ff14028c3890fb7
Mark Skelton <mdskelton99@gmail.com>
2023-01-14 20:59:37 -0600

    no-focused-test

--- test/spec/no-focused-test.spec.ts
+++ test/spec/no-focused-test.spec.ts
@@ -1,55 +1,151 @@
 import { runRuleTester } from '../utils/rule-tester';
 import rule from '../../src/rules/no-focused-test';
 
-const invalid = (code: string, output: string) => ({
-  code,
-  errors: [
-    {
-      messageId: 'noFocusedTest',
-      suggestions: [{ messageId: 'suggestRemoveOnly', output }],
-    },
-  ],
-});
+const messageId = 'noFocusedTest';
 
 runRuleTester('no-focused-test', rule, {
   invalid: [
-    invalid(
-      'test.describe.only("skip this describe", () => {});',
-      'test.describe("skip this describe", () => {});'
-    ),
-    invalid(
-      'test.describe["only"]("skip this describe", () => {});',
-      'test.describe("skip this describe", () => {});'
-    ),
-    invalid(
-      'test["describe"][`only`]("skip this describe", () => {});',
-      'test["describe"]("skip this describe", () => {});'
-    ),
-    invalid(
-      'test.describe.parallel.only("skip this describe", () => {});',
-      'test.describe.parallel("skip this describe", () => {});'
-    ),
-    invalid(
-      'test.describe.serial.only("skip this describe", () => {});',
-      'test.describe.serial("skip this describe", () => {});'
-    ),
-    invalid(
-      'test.only("skip this test", async ({ page }) => {});',
-      'test("skip this test", async ({ page }) => {});'
-    ),
-    invalid(
-      'test["only"]("skip this test", async ({ page }) => {});',
-      'test("skip this test", async ({ page }) => {});'
-    ),
-    invalid(
-      'test[`only`]("skip this test", async ({ page }) => {});',
-      'test("skip this test", async ({ page }) => {});'
-    ),
+    {
+      code: 'test.describe.only("skip this describe", () => {});',
+      errors: [
+        {
+          messageId,
+          suggestions: [
+            {
+              messageId: 'suggestRemoveOnly',
+              output: 'test.describe("skip this describe", () => {});',
+            },
+          ],
+          line: 1,
+          column: 15,
+          endColumn: 19,
+        },
+      ],
+    },
+    {
+      code: 'test.describe["only"]("skip this describe", () => {});',
+      errors: [
+        {
+          messageId,
+          suggestions: [
+            {
+              messageId: 'suggestRemoveOnly',
+              output: 'test.describe("skip this describe", () => {});',
+            },
+          ],
+          line: 1,
+          column: 15,
+          endColumn: 21,
+        },
+      ],
+    },
+    {
+      code: 'test["describe"][`only`]("skip this describe", () => {});',
+      errors: [
+        {
+          messageId,
+          suggestions: [
+            {
+              messageId: 'suggestRemoveOnly',
+              output: 'test["describe"]("skip this describe", () => {});',
+            },
+          ],
+          line: 1,
+          column: 18,
+          endColumn: 24,
+        },
+      ],
+    },
+    {
+      code: 'test.describe.parallel.only("skip this describe", () => {});',
+      errors: [
+        {
+          messageId,
+          suggestions: [
+            {
+              messageId: 'suggestRemoveOnly',
+              output: 'test.describe.parallel("skip this describe", () => {});',
+            },
+          ],
+          line: 1,
+          column: 24,
+          endColumn: 28,
+        },
+      ],
+    },
+    {
+      code: 'test.describe.serial.only("skip this describe", () => {});',
+      errors: [
+        {
+          messageId,
+          suggestions: [
+            {
+              messageId: 'suggestRemoveOnly',
+              output: 'test.describe.serial("skip this describe", () => {});',
+            },
+          ],
+          line: 1,
+          column: 22,
+          endColumn: 26,
+        },
+      ],
+    },
+    {
+      code: 'test.only("skip this test", async ({code: page }) => {});',
+      errors: [
+        {
+          messageId,
+          suggestions: [
+            {
+              messageId: 'suggestRemoveOnly',
+              output: 'test("skip this test", async ({code: page }) => {});',
+            },
+          ],
+          line: 1,
+          column: 6,
+          endColumn: 10,
+        },
+      ],
+    },
+    {
+      code: 'test["only"]("skip this test", async ({code: page }) => {});',
+      errors: [
+        {
+          messageId,
+          suggestions: [
+            {
+              messageId: 'suggestRemoveOnly',
+              output: 'test("skip this test", async ({code: page }) => {});',
+            },
+          ],
+          line: 1,
+          column: 6,
+          endColumn: 12,
+        },
+      ],
+    },
+    {
+      code: 'test[`only`]("skip this test", async ({code: page }) => {});',
+      errors: [
+        {
+          messageId,
+          suggestions: [
+            {
+              messageId: 'suggestRemoveOnly',
+              output: 'test("skip this test", async ({code: page }) => {});',
+            },
+          ],
+          line: 1,
+          column: 6,
+          endColumn: 12,
+        },
+      ],
+    },
   ],
   valid: [
     'test.describe("describe tests", () => {});',
     'test.describe.skip("describe tests", () => {});',
-    'test("one", async ({ page }) => {});',
+    'test("one", async ({code: page }) => {});',
     'test.fixme(isMobile, "Settings page does not work in mobile yet");',
     'test["fixme"](isMobile, "Settings page does not work in mobile yet");',
     'test[`fixme`](isMobile, "Settings page does not work in mobile yet");',
@@ -57,7 +153,7 @@ runRuleTester('no-focused-test', rule, {
     'test["slow"]();',
     'test[`slow`]();',
     'const only = true;',
-    'function only() { return null };',
+    'function only() {code: return null };',
     'this.only();',
   ],
 });
