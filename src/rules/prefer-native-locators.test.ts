6a5d92ac6206a73441e2753affa0056c64c9b6f0
Cameron McHenry <camchenry@users.noreply.github.com>
2024-08-18 22:42:42 -0400

    Add more tests

--- src/rules/prefer-native-locators.test.ts
+++ src/rules/prefer-native-locators.test.ts
@@ -39,6 +39,35 @@ runRuleTester('prefer-native-locators', rule, {
       options: [{ testIdAttribute: 'data-custom-testid' }],
       output: 'page.getByTestId("password-input")',
     },
+    // Works when locators are chained
+    {
+      code: `this.page.locator('[role="heading"]').first()`,
+      errors: [{ column: 1, line: 1, messageId: 'unexpectedRoleQuery' }],
+      output: 'this.page.getByRole("heading").first()',
+    },
+    // Works when used inside an assertion
+    {
+      code: `await expect(page.locator('[role="alert"]')).toBeVisible()`,
+      errors: [{ column: 14, line: 1, messageId: 'unexpectedRoleQuery' }],
+      output: 'await expect(page.getByRole("alert")).toBeVisible()',
+    },
+    {
+      code: `await expect(page.locator('[data-testid="top"]')).toContainText(firstRule)`,
+      errors: [{ column: 14, line: 1, messageId: 'unexpectedTestIdQuery' }],
+      output: 'await expect(page.getByTestId("top")).toContainText(firstRule)',
+    },
+    // Works when used as part of an action
+    {
+      code: `await page.locator('[placeholder="New password"]').click()`,
+      errors: [{ column: 7, line: 1, messageId: 'unexpectedPlaceholderQuery' }],
+      output: 'await page.getByPlaceholder("New password").click()',
+    },
+    // Works when it is not page.locator
+    {
+      code: `this.locator('[aria-label="View more"]')`,
+      errors: [{ column: 1, line: 1, messageId: 'unexpectedLabelQuery' }],
+      output: 'this.getByLabel("View more")',
+    },
   ],
   valid: [
     { code: 'page.getByLabel("View more")' },
