2286a832bb621452b46cb9831f0ffb51c9143c6b
Michal Sroczynski <93290723+MSroczynski3@users.noreply.github.com>
2025-07-30 21:59:30 +0200

    Lint

--- src/rules/prefer-web-first-assertions.ts
+++ src/rules/prefer-web-first-assertions.ts
@@ -85,7 +85,7 @@ export default createRule({
         // Playwright method must be supported
         const method = getStringValue(call.callee.property)
         const methodConfig = methods[method]
-        if (!methods.hasOwnProperty(method)) return
+        if (!Object.hasOwn(methods, method)) return
 
         // Change the matcher
         const notModifier = fnCall.modifiers.find(
